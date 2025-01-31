5584eb6521c83cd687d53e4696052fc8db0e22bf
Adam Cain <03adamcain@gmail.com>
2025-01-31 10:10:34 +0000

    feat: state and function of search bar

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,48 +1,82 @@
-import * as React from "react"
+"use client"
+import { useRouter } from "next/navigation";
 
 import {
     Select,
     SelectContent,
     SelectGroup,
     SelectItem,
-    SelectLabel,
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select"
 import { Input } from "@/components/ui/input"
 import { SearchIcon } from "lucide-react"
 import { Button } from "./ui/button"
-
+import { useState } from "react"
 
 export default function Search() {
+    const router = useRouter();
+    const [query, setQuery] = useState("")
+    const [filter, setFilter] = useState<FilterType>(FilterType.ALL);
+
+    const handleSubmit = () => {
+        const searchParams = new URLSearchParams({
+            query,
+            filter,
+        }).toString();
+        console.log(`/search?${searchParams}`);
+        router.push(`/search?${searchParams}`);
+    };
+
     return (
         <div className="flex-inline flex-row flex">
-            <Selector />
-            <Input 
-            className="bg-white rounded-none" 
-            placeholder="Search for "
-            
+            <Selector filter={filter} setFilter={setFilter} />
+            <Input
+                className="bg-white rounded-none"
+                placeholder="Search for "
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                        handleSubmit();
+                    }
+                }}
             />
-            <Button className="rounded-l-none bg-white border border-l-0 z-10 text-gray-600 hover:bg-gray-600 hover:text-white">
+            <Button 
+            onClick={handleSubmit}
+            className="rounded-l-none hover:bg-white border border-l-0 z-10 hover:text-stone-900 bg-stone-900 text-white">
                 <SearchIcon className="text-inherit" />
             </Button>
         </div>
     )
 }
 
-function Selector() {
+interface SelectorProps {
+    filter: string;
+    setFilter: (value: FilterType) => void;
+}
+
+enum FilterType {
+    ALL = "all",
+    USER = "user",
+    GAMES = "games",
+    SESSIONS = "sessions",
+    INTERACTIONS = "interactions",
+}
+
+function Selector({ filter, setFilter }: SelectorProps) {
     return (
-        <Select>
+        <Select value={filter} onValueChange={setFilter}>
             <SelectTrigger className="w-[125px] rounded-r-none border-r-none bg-white">
                 <SelectValue placeholder="All filters" />
             </SelectTrigger>
             <SelectContent>
                 <SelectGroup>
-                    <SelectItem value="apple">All filters</SelectItem>
-                    <SelectItem value="pineapple">User</SelectItem>
-                    <SelectItem value="banana">Games</SelectItem>
-                    <SelectItem value="blueberry">Sessions</SelectItem>
-                    <SelectItem value="grapes">Interactions</SelectItem>
+                    <SelectItem value={FilterType.ALL}>All filters</SelectItem>
+                    <SelectItem value={FilterType.USER}>User</SelectItem>
+                    <SelectItem value={FilterType.GAMES}>Games</SelectItem>
+                    <SelectItem value={FilterType.SESSIONS}>Sessions</SelectItem>
+                    <SelectItem value={FilterType.INTERACTIONS}>Interactions</SelectItem>
                 </SelectGroup>
             </SelectContent>
         </Select>

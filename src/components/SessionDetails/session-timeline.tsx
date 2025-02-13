6ff5630b38fa3d36f73ee5b946a4218db65567c0
Adam Cain <03adamcain@gmail.com>
2025-02-13 10:10:17 +0000

    feat: added not found to session timeline

--- src/components/SessionDetails/session-timeline.tsx
+++ src/components/SessionDetails/session-timeline.tsx
@@ -3,6 +3,7 @@ import { UserPlus, UserMinus } from 'lucide-react'
 import { SessionEvent } from '@/types'
 import { SessionDetailsChildrenProps } from ".";
 import PaginationControls from '../PaginationControls';
+import NotFoundElement from '../NotFoundElement';
 export default function SessionTimeline({ data, onQueryChange, query }: SessionDetailsChildrenProps<SessionEvent>) {
 
   const handleNextPrev = (num: number) => {
@@ -19,13 +20,16 @@ export default function SessionTimeline({ data, onQueryChange, query }: SessionD
   return (
     <div className="bg-white shadow-md rounded-lg p-4 sm:p-8">
       <h2 className="text-xl font-semibold mb-5">Session Timeline</h2>
-      <div className="relative">
-        <div className="absolute top-0 bottom-0 left-4 w-0.5 bg-stone-300">
+      {data.total === 0 ? (
+        <NotFoundElement message="No events found" />
+      ) : (
+        <div>
+        <div className="relative">
+          <div className="absolute top-0 bottom-0 left-4 w-0.5 bg-stone-300">
           <div className="absolute -top-2 left-1/2 -translate-x-1/2 size-2 rounded-full bg-white border-2 border-stone-300" />
           <div className="absolute -bottom-1 left-1/2 -translate-x-1/2 size-2 rounded-full bg-white border-2 border-stone-300" />
         </div>
-        {/* A NotFoundElement is not needed here since events will always exist for a valid session */}
-
+        
         {data.data.map((event) => (
           <div key={event.id} className="pb-3 flex items-center overflow-clip">
             <div className={`w-8 h-8 min-w-8 min-h-8 rounded-full flex items-center justify-center z-10 ${event.event === 'JOIN' ? 'bg-green-500' : 'bg-red-500'
@@ -56,6 +60,8 @@ export default function SessionTimeline({ data, onQueryChange, query }: SessionD
           setPageSize={(num: number) => onQueryChange({ ...query, limit: num })}    
           nextPage={() => handleNextPrev(1)}
           previousPage={() => handleNextPrev(-1)} />
+        </div>
+      )}
     </div>
   )
 }
